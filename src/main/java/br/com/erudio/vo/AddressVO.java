d560fbf750d05360b55db5578d0fbfcd36b76e07
Leandro da Costa Gonçalves <leandrocgsi@gmail.com>
2016-02-20 23:30:59 -0200

    Removing unused imports

--- src/main/java/br/com/erudio/vo/AddressVO.java
+++ src/main/java/br/com/erudio/vo/AddressVO.java
@@ -2,23 +2,6 @@ package br.com.erudio.vo;
 
 import java.io.Serializable;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-
-import org.hibernate.annotations.ForeignKey;
-
-import br.com.erudio.vo.AddressVO;
-import br.com.erudio.vo.AddressTypeVO;
-import br.com.erudio.vo.CityVO;
-import br.com.erudio.vo.PublicAreaTypeVO;
-import br.com.erudio.vo.StateVO;
-
 public class AddressVO implements Serializable {
     
     private static final long serialVersionUID = 1L;
--- src/main/java/br/com/erudio/vo/PublicAreaTypeVO.java
+++ src/main/java/br/com/erudio/vo/PublicAreaTypeVO.java
@@ -2,12 +2,6 @@ package br.com.erudio.vo;
 
 import java.io.Serializable;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
 public class PublicAreaTypeVO implements Serializable {
     
     private static final long serialVersionUID = 1L;
