cf790668c0982ce48aefcb1a0ba89bcf8c9d1b8c
Leandro da Costa Gonçalves <leandrocgsi@gmail.com>
2016-02-20 23:14:29 -0200

    We still have problems with Groovy

--- src/main/groovy/br/com/erudio/dto/PagedSearchDTO.groovy
+++ src/main/groovy/br/com/erudio/dto/PagedSearchDTO.groovy
@@ -5,7 +5,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.Query
 
-import org.apache.commons.lang.StringUtils
+import org.apache.commons.lang3.StringUtils
 
 
 public class PagedSearchDTO<T extends Serializable> implements Serializable {
