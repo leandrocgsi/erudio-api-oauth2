d10ad8820e678470ba5dbcb12ad2aa2cd23ec1d1
Leandro da Costa Gonçalves <leandrocgsi@gmail.com>
2016-04-25 00:07:17 -0300

    Adding parser VO to Entity for CityEntryPoint

--- src/main/java/br/com/erudio/entrypoint/v1/CityEntryPoint.java
+++ src/main/java/br/com/erudio/entrypoint/v1/CityEntryPoint.java
@@ -17,8 +17,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 
+import br.com.erudio.converter.ObjectParser;
 import br.com.erudio.model.City;
 import br.com.erudio.repository.interfaces.ICityRepository;
+import br.com.erudio.vo.CityVO;
 
 @Controller
 @Secured("ROLE_USER")
@@ -32,15 +34,17 @@ class CityEntryPoint {
 	@RequestMapping(method = RequestMethod.POST)
     @ResponseStatus(value = HttpStatus.OK)
 	@ApiOperation(httpMethod = "POST", value = "Saving a city")
-	public @ResponseBody City save(@RequestBody City city) {
-		return cityRepository.save(city);
+	public @ResponseBody CityVO save(@RequestBody CityVO city) {
+		City savedCity = cityRepository.save(ObjectParser.parseObjectInputToObjectOutput(city, City.class));
+		return ObjectParser.parseObjectInputToObjectOutput(savedCity, CityVO.class);
 	}
 
 	@RequestMapping(method = RequestMethod.PUT)
     @ResponseStatus(value = HttpStatus.OK)
 	@ApiOperation(httpMethod = "PUT", value = "Updating a city")
-	public @ResponseBody City update(@RequestBody City city) {
-		return cityRepository.update(city);
+	public @ResponseBody CityVO update(@RequestBody CityVO city) {
+		City updatedCity = cityRepository.update(ObjectParser.parseObjectInputToObjectOutput(city, City.class));
+		return ObjectParser.parseObjectInputToObjectOutput(updatedCity, CityVO.class);
 	}
     
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
@@ -54,21 +58,24 @@ class CityEntryPoint {
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	@ResponseStatus(value = HttpStatus.OK)
 	@ApiOperation(httpMethod = "GET", value = "Find a city by id")
-	public @ResponseBody City findById(@PathVariable Integer id) {
-		return cityRepository.findById(id);
+	public @ResponseBody CityVO findById(@PathVariable Integer id) {
+		City city = cityRepository.findById(id);
+		return ObjectParser.parseObjectInputToObjectOutput(city, CityVO.class);
 	}
 	
 	@RequestMapping(value = "/findByName/{name}", method = RequestMethod.GET)
     @ResponseStatus(value = HttpStatus.OK)
 	@ApiOperation(httpMethod = "GET", value = "Find a city by name")
-    public @ResponseBody City findCityByName(@PathVariable String name) {
-        return cityRepository.findByName(name);
+    public @ResponseBody CityVO findCityVOByName(@PathVariable String name) {
+        City city = cityRepository.findByName(name);
+		return ObjectParser.parseObjectInputToObjectOutput(city, CityVO.class);
     }
     
 	@RequestMapping(value = "/findAll", method = RequestMethod.GET)
     @ResponseStatus(value = HttpStatus.OK)
 	@ApiOperation(httpMethod = "GET", value = "Find all cities")
-    public @ResponseBody List<City> findAll() {
-        return cityRepository.findAll();
+    public @ResponseBody List<CityVO> findAll() {
+        List<City> allCities = cityRepository.findAll();
+		return ObjectParser.parserListObjectInputToObjectOutput(allCities, CityVO.class);
     }
 }
