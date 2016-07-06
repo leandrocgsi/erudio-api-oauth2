122d08500eb9644060ac3f8f7c2a88306641d756
leandrocgsi <leandrocgsi@gmail.com>
2016-07-06 00:10:35 -0300

    Adding new operation in CountryEntryPoint

--- src/main/java/br/com/erudio/entrypoint/v1/CountryEntryPoint.java
+++ src/main/java/br/com/erudio/entrypoint/v1/CountryEntryPoint.java
@@ -1,5 +1,8 @@
 package br.com.erudio.entrypoint.v1;
 
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -8,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -29,49 +33,18 @@ public class CountryEntryPoint {
 	@Autowired
     private ICountryRepository countryRepository;
     
-	/*@RequestMapping(method = RequestMethod.POST)
-    @ResponseStatus(value = HttpStatus.OK)
-	@ApiOperation(httpMethod = "POST", value = "Saving a country")
-	public @ResponseBody CountryVO save(@RequestBody CountryVO country) {
-		country.setInsertDate(new Date());
-		country.setIdUserInsert(0);
-		country.setActive(true);
-		Country savedCountry = countryRepository.save(ObjectParser.parseObjectInputToObjectOutput(country, Country.class));
-		CountryVO countryVO = ObjectParser.parseObjectInputToObjectOutput(savedCountry, CountryVO.class);
-		addHATEOASSupport(countryVO);
-		return countryVO;
-	}
-
-	@RequestMapping(method = RequestMethod.PUT)
-    @ResponseStatus(value = HttpStatus.OK)
-	@ApiOperation(httpMethod = "PUT", value = "Updating a country")
-	public @ResponseBody CountryVO update(@RequestBody CountryVO country) {
-		country.setUpdatedDate(new Date());
-		country.setIdUserUpdate(0);
-		Country updatedCountry = countryRepository.update(ObjectParser.parseObjectInputToObjectOutput(country, Country.class));
-		CountryVO countryVO = ObjectParser.parseObjectInputToObjectOutput(updatedCountry, CountryVO.class);
-		addHATEOASSupport(countryVO);
-		return cityVO;
-	}
-    
-	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    @ResponseStatus(value = HttpStatus.OK)
-	@ApiOperation(httpMethod = "DELETE", value = "Deleting a country by id")
-    public @ResponseBody ResponseEntity<Void> delete(@PathVariable Integer id) {
-		countryReposiountryy.deleteById(id);
-		return new ResponseEntity<Void>(HttpStatus.OK);
-	}
-
+	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	@ResponseStatus(value = HttpStatus.OK)
 	@ApiOperation(httpMethod = "GET", value = "Find a country by id")
 	public @ResponseBody CountryVO findById(@PathVariable Integer id) {
 		Country country = countryRepository.findById(id);
-		CountryVO countryVO = ObjectParser.parseObjectInputToObjectOutput(country, CountryVO.class);
+		CountryVO countryVO = parseCountry(country);
 		addHATEOASSupport(countryVO);
 		return countryVO;
 	}
 	
+	/*
 	@RequestMapping(value = "/findByName/{name}", method = RequestMethod.GET)
     @ResponseStatus(value = HttpStatus.OK)
 	@ApiOperation(httpMethod = "GET", value = "Find a country by name")
@@ -88,24 +61,29 @@ public class CountryEntryPoint {
     public @ResponseBody List<CountryVO> findAll() {
         List<Country> allCountries = countryRepository.findAll();
 		List<CountryVO> citiesVO = parseCountries(allCountries);
-//		for (CountryVO countryVO : citiesVO) addHATEOASSupport(countryVO);
+		for (CountryVO countryVO : citiesVO) addHATEOASSupport(countryVO);
 		return citiesVO;
     }
 
 	private List<CountryVO> parseCountries(List<Country> allCountries) {
 		ArrayList<CountryVO> countries = new ArrayList<>();
 		for (Country country : allCountries) {
-			CountryVO countryVO = new CountryVO();
-			countryVO.setIdCountry(country.getIdCountry());
-			countryVO.setLocaleMessageKey(country.getLocaleMessageKey());
-			countryVO.setName(country.getName());
-			
-			String[] states = country.getStates().split("\\|");
-			
-			countryVO.setStates(Arrays.asList(states));
+			CountryVO countryVO = parseCountry(country);
 			countries.add(countryVO);
 		}
 		return countries;
+	}
+
+	private CountryVO parseCountry(Country country) {
+		CountryVO countryVO = new CountryVO();
+		countryVO.setIdCountry(country.getIdCountry());
+		countryVO.setLocaleMessageKey(country.getLocaleMessageKey());
+		countryVO.setName(country.getName());
+		
+		String[] states = country.getStates().split("\\|");
+		
+		countryVO.setStates(Arrays.asList(states));
+		return countryVO;
 	}
 
 		/*@Test
@@ -116,7 +94,7 @@ public class CountryEntryPoint {
 			}
 			List<String> myList = new ArrayList<String>(Arrays.asList(ary));
 		}*/
-	/*private void addHATEOASSupport(CountryVO countryVO) {
+	private void addHATEOASSupport(CountryVO countryVO) {
 		countryVO.add(linkTo(methodOn(CountryEntryPoint.class).findById(countryVO.getIdCountry())).withSelfRel());
-	}*/
+	}
 }
