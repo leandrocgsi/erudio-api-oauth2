941818909955f66e862de5b769722398c3714f26
leandrocgsi <leandrocgsi@gmail.com>
2016-07-30 23:57:35 -0300

    Creating your own Exception

---
+++ src/main/java/exceptions/BaseException.java
@@ -0,0 +1,22 @@
+package exceptions;
+
+public class BaseException extends Exception {
+    
+    private static final long serialVersionUID = 1L;
+
+    public BaseException() {
+        super();
+    }
+
+    public BaseException(String message) {
+        super(message);
+    }
+
+    public BaseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BaseException(Throwable cause) {
+        super(cause);
+    }
+}
