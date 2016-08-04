7ed66882c933f2cb77f56daf68901dfb67517ea3
leandrocgsi <leandrocgsi@gmail.com>
2016-08-04 00:02:05 -0300

    Creating your own Exceptions

--- src/main/java/exceptions/FileImportException.java
+++ src/main/java/exceptions/FileImportException.java
@@ -1,6 +1,6 @@
 package exceptions;
 
-public class WebServiceAccessException extends BaseException{
+public class FileImportException extends BaseException{
 
     private static final long serialVersionUID = 1L;
 
