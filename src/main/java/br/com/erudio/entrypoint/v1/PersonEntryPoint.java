8184c8100e6f66c58eaa89ed71f424cb89eff744
leandrocgsi <leandrocgsi@gmail.com>
2016-08-12 20:45:43 -0300

    Disable local CORS

--- src/main/java/br/com/erudio/entrypoint/v1/PersonEntryPoint.java
+++ src/main/java/br/com/erudio/entrypoint/v1/PersonEntryPoint.java
@@ -11,7 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,81 +33,80 @@ import br.com.erudio.vo.PersonVO;
 @RequestMapping("/api/v1/person")
 class PersonEntryPoint {
 
-	@Autowired
-	private IPersonRepository personRepository;
+    @Autowired
+    private IPersonRepository personRepository;
     
-	@RequestMapping(method = RequestMethod.POST)
+    @RequestMapping(method = RequestMethod.POST)
     @ResponseStatus(value = HttpStatus.OK)
-	@ApiOperation(httpMethod = "POST", value = "Insert a new person")
-	public @ResponseBody PersonVO save(@RequestBody Person person) {
-		person.setIdUserInsert(0);
-		person.setInsertDate(new Date());
-		person.setActive(true);
-		Person personEntity = ObjectParser.parseObjectInputToObjectOutput(person, Person.class);
-		personEntity = personRepository.save(personEntity);
-		PersonVO personVO = ObjectParser.parseObjectInputToObjectOutput(personEntity, PersonVO.class);
-		addHATEOASSupport(personVO);
-		return personVO;
-	}
+    @ApiOperation(httpMethod = "POST", value = "Insert a new person")
+    public @ResponseBody PersonVO save(@RequestBody Person person) {
+        person.setIdUserInsert(0);
+        person.setInsertDate(new Date());
+        person.setActive(true);
+        Person personEntity = ObjectParser.parseObjectInputToObjectOutput(person, Person.class);
+        personEntity = personRepository.save(personEntity);
+        PersonVO personVO = ObjectParser.parseObjectInputToObjectOutput(personEntity, PersonVO.class);
+        addHATEOASSupport(personVO);
+        return personVO;
+    }
 
-	@RequestMapping(method = RequestMethod.PUT)
+    @RequestMapping(method = RequestMethod.PUT)
     @ResponseStatus(value = HttpStatus.OK)
-	@ApiOperation(httpMethod = "PUT", value = "Update person")
-	public @ResponseBody PersonVO update(@RequestBody Person person) {
-		person.setIdUserUpdate(0);
-		person.setUpdatedDate(new Date());
-		Person personEntity = ObjectParser.parseObjectInputToObjectOutput(person, Person.class);
-		personEntity = personRepository.update(personEntity);
-		PersonVO personVO = ObjectParser.parseObjectInputToObjectOutput(personEntity, PersonVO.class);
-		addHATEOASSupport(personVO);
-		return personVO;
-	}
+    @ApiOperation(httpMethod = "PUT", value = "Update person")
+    public @ResponseBody PersonVO update(@RequestBody Person person) {
+        person.setIdUserUpdate(0);
+        person.setUpdatedDate(new Date());
+        Person personEntity = ObjectParser.parseObjectInputToObjectOutput(person, Person.class);
+        personEntity = personRepository.update(personEntity);
+        PersonVO personVO = ObjectParser.parseObjectInputToObjectOutput(personEntity, PersonVO.class);
+        addHATEOASSupport(personVO);
+        return personVO;
+    }
     
-	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     @ResponseStatus(value = HttpStatus.OK)
-	@ApiOperation(httpMethod = "DELETE", value = "Delete persons by id")
+    @ApiOperation(httpMethod = "DELETE", value = "Delete persons by id")
     public @ResponseBody ResponseEntity<Void> delete(@PathVariable Integer id) {
-		personRepository.deleteById(id);
-		return ResponseEntity.status(HttpStatus.OK).build();
-	}
+        personRepository.deleteById(id);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
 
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	@ResponseStatus(value = HttpStatus.OK)
-	@ApiOperation(httpMethod = "GET", value = "Find persons by id")
-	public @ResponseBody PersonVO findById(@PathVariable Integer id) {
-		Person personEntity = personRepository.findById(id);
-		PersonVO personVO = ObjectParser.parseObjectInputToObjectOutput(personEntity, PersonVO.class);
-		addHATEOASSupport(personVO);
-		return personVO;
-	}
-	
-	@RequestMapping(value = "/findByName/{name}", method = RequestMethod.GET)
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     @ResponseStatus(value = HttpStatus.OK)
-	@ApiOperation(httpMethod = "GET", value = "Find persons by name")
-    public @ResponseBody PersonVO findPersonByName(@PathVariable String name) {
-		Person personEntity = personRepository.findByName(name);
-		PersonVO personVO = ObjectParser.parseObjectInputToObjectOutput(personEntity, PersonVO.class);
-		addHATEOASSupport(personVO);
-		return personVO;
+    @ApiOperation(httpMethod = "GET", value = "Find persons by id")
+    public @ResponseBody PersonVO findById(@PathVariable Integer id) {
+        Person personEntity = personRepository.findById(id);
+        PersonVO personVO = ObjectParser.parseObjectInputToObjectOutput(personEntity, PersonVO.class);
+        addHATEOASSupport(personVO);
+        return personVO;
     }
     
-	@RequestMapping(value = "/findAll", method = RequestMethod.GET)
+    @RequestMapping(value = "/findByName/{name}", method = RequestMethod.GET)
     @ResponseStatus(value = HttpStatus.OK)
-	@ApiOperation(httpMethod = "GET", value = "Find all persons")
-    public @ResponseBody List<PersonVO> findAll() {
-		List<Person> allPersons = personRepository.findAll();
-		List<PersonVO> personsVO = ObjectParser.parserListObjectInputToObjectOutput(allPersons, PersonVO.class);
-		for (PersonVO personVO : personsVO) addHATEOASSupport(personVO);
-		return personsVO;
+    @ApiOperation(httpMethod = "GET", value = "Find persons by name")
+    public @ResponseBody PersonVO findPersonByName(@PathVariable String name) {
+        Person personEntity = personRepository.findByName(name);
+        PersonVO personVO = ObjectParser.parseObjectInputToObjectOutput(personEntity, PersonVO.class);
+        addHATEOASSupport(personVO);
+        return personVO;
     }
-	
-	@CrossOrigin(origins = "http://localhost:9000")
-	@RequestMapping(value = "/pagedSearch", method = RequestMethod.POST)
+    
+    @RequestMapping(value = "/findAll", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    @ApiOperation(httpMethod = "GET", value = "Find all persons")
+    public @ResponseBody List<PersonVO> findAll() {
+        List<Person> allPersons = personRepository.findAll();
+        List<PersonVO> personsVO = ObjectParser.parserListObjectInputToObjectOutput(allPersons, PersonVO.class);
+        for (PersonVO personVO : personsVO) addHATEOASSupport(personVO);
+        return personsVO;
+    }
+    
+    @RequestMapping(value = "/pagedSearch", method = RequestMethod.POST)
     public @ResponseBody PagedSearchDTO<PersonVO> buscaPaginada(@RequestBody PagedSearchDTO<PersonVO> person) {
-		return personRepository.pagedSearch(person);
+        return personRepository.pagedSearch(person);
     }
 
-	private void addHATEOASSupport(PersonVO personVO) {
-		personVO.add(linkTo(methodOn(PersonEntryPoint.class).findById(personVO.getIdPerson())).withSelfRel());
-	}
+    private void addHATEOASSupport(PersonVO personVO) {
+        personVO.add(linkTo(methodOn(PersonEntryPoint.class).findById(personVO.getIdPerson())).withSelfRel());
+    }
 }
