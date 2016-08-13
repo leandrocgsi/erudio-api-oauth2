6a2ce782c05bdf358f8f3d54485fd98e0c248668
leandrocgsi <leandrocgsi@gmail.com>
2016-08-13 20:46:26 -0300

    Enable global CORS

---
+++ src/main/java/br/com/erudio/configuration/cors/CORSConfig.java
@@ -0,0 +1,23 @@
+package br.com.erudio.configuration.cors;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableAutoConfiguration
+public class CORSConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/v1/**").allowedOrigins("http://localhost:9000");
+            }
+        };
+    }
+}
